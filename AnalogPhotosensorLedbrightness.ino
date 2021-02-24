91ce433ec7656775ba779776c5de92a3a050200c
Adnan Sattikar <adnan.sattikar2001@gmail.com>
2021-02-24 11:48:47 +0530

    Modified code

--- AnalogPhotosensorLedbrightness.ino
+++ AnalogPhotosensorLedbrightness.ino
@@ -1,19 +1,39 @@
+/*
+  Analog input, analog output, serial output
+
+  Reads an analog input pin, maps the result to a
+  range from 0 to 255  and uses the result to set
+  the pulsewidth modulation (PWM) of an output
+  pin.  Also prints the results to the serial
+  monitor.
+
+  The circuit:
+  * potentiometer connected to analog pin 0.
+    Center pin of the potentiometer goes to the
+    analog pin. side pins of the potentiometer go
+    to +5V and ground
+  * LED connected from digital pin 9 to ground
+
+*/
 int sensePin =0;
-int ledPin = 9;
+int ledPin =9;
+
 void setup()
 {
- analogReference(DEFAULT);
-  
+  analogReference(DEFAULT);
   pinMode(ledPin,OUTPUT);
     
 }
 
 void loop()
 {
- int val = analogRead(sensepin);
+  // read the analog in value:
+  int val = analogRead(sensePin);
   val = constrain(val,750,900);
-  
-  int ledlevel =  map(val,750,900,255,0);
-  
- analogWrite(ledPin,ledlevel);
+  // map it to the range of the analog out:
+  int ledLevel = map(val, 750, 900, 255, 0);
+  // change the analog out value:
+  analogWrite(ledPin, ledLevel);
+ 
+  delay(2); // Wait for 2 millisecond(s)
 }
